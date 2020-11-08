5be8970ffe3ef1760dc1825f6a02f6643157c425
Eugene Mitikari <accakyra@inbox.ru>
2020-11-08 14:33:59 +0700

    unpredictable behavior by violating JMM rules

---
+++ src/cache_lines/Volatile.java
@@ -0,0 +1,21 @@
+package cache_lines;
+
+public class Volatile {
+    /**
+     * According to JMM this program can run forever
+     * because JMM doesn't know something about cache coherence.
+     * But nowadays processors <b>probably<b/> has cache coherence protocol.
+     */
+
+    static /*volatile*/ boolean flag = true;
+
+    public static void main(String[] args) {
+        new Thread(() -> {
+            while (flag) {
+                // NOP
+            }
+        }).start();
+
+        flag = false;
+    }
+}
